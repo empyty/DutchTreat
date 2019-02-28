e756bda8c0385da68cc2535d0bd3c5e51e72004c
empyty <bartoszlistwan@gmail.com>
2019-02-28 17:52:57 +0100

    Added appController

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,49 @@
+﻿using DutchTreat.Models;
+using DutchTreat.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService mailService;
+
+        public AppController(IMailService mailService)
+        {
+            this.mailService = mailService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Send the email
+                mailService.SendMessage(
+                    "itsameluigi666@gmail.com", model.Subject,
+                    $"From: {model.Name} - {model.Message}");
+                ViewBag.UserMessage = "Mail sent";
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+            return View();
+        }
+    }
+}
