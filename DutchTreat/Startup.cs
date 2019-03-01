4ecf0fef919eca2684d18dac8e27af4a01bd1261
empyty <bartoszlistwan@gmail.com>
2019-03-01 14:27:14 +0100

    Added use of configuration file

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -1,16 +1,35 @@
-﻿using DutchTreat.Services;
+﻿using DutchTreat.Data;
+using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DutchTreat
 {
     public class Startup
     {
+        private readonly IConfiguration config;
+
+        public Startup(IConfiguration config)
+        {
+            this.config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DutchContext>(cfg =>
+            {
+                cfg.UseSqlServer(config.GetConnectionString("DutchConnectionString"));
+            });
+
+            services.AddTransient<DutchSeeder>();
+
+            services.AddScoped<IDutchRepository, DutchRepository>();
+
             services.AddTransient<IMailService, NullMailService>();
 
             services.AddMvc();
