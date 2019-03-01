4236af0f51a9ccc6123f2aafe0a32da993d5417b
empyty <bartoszlistwan@gmail.com>
2019-03-01 14:29:03 +0100

    Added repository

---
+++ DutchTreat/Data/DutchRepository.cs
@@ -0,0 +1,40 @@
+﻿using DutchTreat.Data.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace DutchTreat.Data
+{
+    public class DutchRepository : IDutchRepository
+    {
+        private readonly DutchContext context;
+        private readonly ILogger<DutchRepository> logger;
+
+        public DutchRepository(DutchContext context, ILogger<DutchRepository> logger)
+        {
+            this.context = context;
+            this.logger = logger;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            logger.LogInformation("GetAllProducts was called");
+
+            return context.Products
+                .OrderBy(p => p.Title)
+                .ToList();
+        }
+
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return context.Products
+                .Where(p => p.Category == category)
+                .ToList();
+        }
+
+        public bool SaveChanges()
+        {
+            return context.SaveChanges() > 0;
+        }
+    }
+}
