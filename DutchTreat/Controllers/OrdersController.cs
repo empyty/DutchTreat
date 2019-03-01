28353d2e8effd6fcb61c5141b9bfda35232f85f8
empyty <bartoszlistwan@gmail.com>
2019-03-01 15:47:40 +0100

    Added controller for orders

---
+++ DutchTreat/Controllers/OrdersController.cs
@@ -0,0 +1,56 @@
+﻿using DutchTreat.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace DutchTreat.Controllers
+{
+    [Route("api/[Controller]")]
+    public class OrdersController : Controller
+    {
+        private readonly IDutchRepository repository;
+        private readonly ILogger<OrdersController> logger;
+
+        public OrdersController(IDutchRepository repository, ILogger<OrdersController> logger)
+        {
+            this.repository = repository;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(repository.GetAllOrders());
+            }
+            catch (Exception e)
+            {
+                logger.LogError($"Failed to get orders: {e}");
+                return BadRequest("Failed to get orders");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                var order = repository.GetOrderById(id);
+                if (order != null)
+                {
+                    return Ok(order);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception e)
+            {
+                logger.LogError($"Failed to get order: {e}");
+                return BadRequest("Failed to get order");
+            }
+        }
+    }
+}
