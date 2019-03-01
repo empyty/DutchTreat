8878a39238dd6ebcd0e1d910ce5155d8f293edfb
empyty <bartoszlistwan@gmail.com>
2019-03-01 14:29:32 +0100

    Seeder to give database some basic data

---
+++ DutchTreat/Data/DutchSeeder.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DutchTreat.Data.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
+
+namespace DutchTreat.Data
+{
+    public class DutchSeeder
+    {
+        private readonly DutchContext context;
+        private readonly IHostingEnvironment hosting;
+
+        public DutchSeeder(DutchContext context, IHostingEnvironment hosting)
+        {
+            this.context = context;
+            this.hosting = hosting;
+        }
+
+        public void Seed()
+        {
+            context.Database.EnsureCreated();
+
+            if (!context.Products.Any())
+            {
+                var filepath = Path.Combine(hosting.ContentRootPath, "Data/art.json");
+                var json = File.ReadAllText(filepath);
+                var products = JsonConvert.DeserializeObject<IEnumerable<Product>>(json);
+                context.Products.AddRange(products);
+
+                var order = context.Orders.Where(o => o.Id == 1).FirstOrDefault();
+                if (order != null)
+                {
+                    order.Items = new List<OrderItem>();
+                    {
+                        new OrderItem()
+                        {
+                            Product = products.First(),
+                            Quantity = 5,
+                            UnitPrice = products.First().Price
+                        };
+                    }
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+}
