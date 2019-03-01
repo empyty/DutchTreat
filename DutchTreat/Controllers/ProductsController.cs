7d54fd16dfd87992fa65a163fba92452c0c1d67e
empyty <bartoszlistwan@gmail.com>
2019-03-01 15:47:31 +0100

    Added controller for products

---
+++ DutchTreat/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using DutchTreat.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using DutchTreat.Data.Entities;
+
+namespace DutchTreat.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IDutchRepository repository;
+        private readonly ILogger<ProductsController> logger;
+
+        public ProductsController(IDutchRepository repository, ILogger<ProductsController> logger)
+        {
+            this.repository = repository;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            try
+            {
+                return Ok(repository.GetAllProducts());
+            }
+            catch (Exception e)
+            {
+                logger.LogError($"Failed to get all products: {e}");
+                return BadRequest("Failed to get all products");
+            }
+        }
+    }
+}
