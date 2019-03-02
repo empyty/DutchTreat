89aa3dc0d4a65f1e2e71ef9af612e11a5e7628c4
empyty <bartoszlistwan@gmail.com>
2019-03-02 22:32:41 +0100

    Added basic app module with angular

---
+++ DutchTreat/ClientApp/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-shop",
+    templateUrl: "./app.component.html",
+    styles: []
+})
+export class AppComponent {
+    title = "Dutch Treat";
+}
