0aef13fc8a62303e6ff3f7a257fae7b98be21366
empyty <bartoszlistwan@gmail.com>
2019-03-02 12:40:21 +0100

    Added orderModel for mapping

---
+++ DutchTreat/Models/OrderModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DutchTreat.Models
+{
+    public class OrderModel
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        [Required]
+        [MinLength(4)]
+        public string OrderNumber { get; set; }
+    }
+}
