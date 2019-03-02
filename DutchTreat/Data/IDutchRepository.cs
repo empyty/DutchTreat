004e3b9da962fe5a5ee27c3d561a413a5623ce82
empyty <bartoszlistwan@gmail.com>
2019-03-02 17:55:34 +0100

    Added method to get orders by user

--- DutchTreat/Data/IDutchRepository.cs
+++ DutchTreat/Data/IDutchRepository.cs
@@ -9,7 +9,8 @@ namespace DutchTreat.Data
         IEnumerable<Product> GetProductsByCategory(string category);
 
         IEnumerable<Order> GetAllOrders(bool includeItems);
-        Order GetOrderById(int id);
+        IEnumerable<Order> GetAllOrdersByUser(string username, bool includeItems);
+        Order GetOrderById(string name, int id);
 
         bool SaveAll();
         void AddEntity(object model);
