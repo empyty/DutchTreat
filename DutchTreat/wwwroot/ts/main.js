558ce97b7ee77431f472a2aa07877e0582c28a94
empyty <bartoszlistwan@gmail.com>
2019-03-02 18:16:43 +0100

    Added typescript files for practice

---
+++ DutchTreat/wwwroot/ts/main.js
@@ -0,0 +1,3 @@
+var shopper = new StoreCustomer("Mario", "Mario");
+shopper.showName();
+//# sourceMappingURL=main.js.map
---
+++ DutchTreat/wwwroot/ts/storecustomer.ts
@@ -0,0 +1,21 @@
+﻿class StoreCustomer {
+
+    constructor(private firstName: string, private lastName: string) {
+
+    }
+
+    public visits: number = 0;
+    private ourName: string;
+
+    public showName() {
+        alert(this.firstName + " " + this.lastName);
+    }
+
+    set name(value) {
+        this.ourName = value;
+    }
+
+    get name() {
+        return this.ourName;
+    }
+}
