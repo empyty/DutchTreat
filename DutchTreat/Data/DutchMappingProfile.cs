c59a95d38e0b314f6fc09da3b06a74de3c7bdc39
empyty <bartoszlistwan@gmail.com>
2019-03-02 12:41:11 +0100

    Specified mapping profile, because of different names in classes

---
+++ DutchTreat/Data/DutchMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DutchTreat.Data.Entities;
+using DutchTreat.Models;
+
+namespace DutchTreat.Data
+{
+    public class DutchMappingProfile : Profile
+    {
+        public DutchMappingProfile()
+        {
+            CreateMap<Order, OrderModel>()
+                .ForMember(o => o.OrderId,
+                    ex => ex.MapFrom(o => o.Id))
+                .ReverseMap();
+        }
+    }
+}
