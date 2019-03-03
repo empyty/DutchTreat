fe64b19bedad31bff89d0650dd95a2ca3d107fa8
empyty <bartoszlistwan@gmail.com>
2019-03-03 21:43:27 +0100

    Angular shared components

---
+++ DutchTreat/ClientApp/app/shared/order.ts
@@ -0,0 +1,24 @@
+﻿import * as _ from "lodash";
+
+export class Order {
+    orderId: number;
+    orderDate = new Date();
+    orderNumber: string;
+    items = new Array<OrderItem>();
+
+    get subtotal(): number {
+        return _.sum(_.map(this.items, i => i.unitPrice * i.quantity));
+    };
+}
+
+export class OrderItem {
+    id: number;
+    quantity: number;
+    unitPrice: number;
+    productId: number;
+    productCategory: string;
+    productSize: string;
+    productTitle: string;
+    productArtist: string;
+    productArtId: string;
+}
